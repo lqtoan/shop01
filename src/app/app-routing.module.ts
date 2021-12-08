5335f24785ab107ccec30133cfe4c70be26120aa
lqtoan <lqtoan37@gmail.com>
2021-12-08 23:13:14 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
+import { ProductListComponent } from './modules/product/product-list/product-list.component';
 import { AboutComponent } from './components/about/about.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'shop',
+    component: ProductListComponent,
+  },
+  {
     path: 'about',
     component: AboutComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'shop',
+    pathMatch: 'full',
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutComponent } from './components/about/about.component';
+import { ProductListComponent } from './modules/product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    AboutComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
