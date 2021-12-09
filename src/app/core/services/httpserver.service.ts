b8e0787c5b8bfd9da254b370f1f22a7cfe9ba92d
lqtoan <lqtoan37@gmail.com>
2021-12-09 09:03:18 +0700

    update

---
+++ src/app/core/services/httpserver.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServerService {
+  private PRODUCT_API = 'http://localhost:3000/products';
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    }),
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getProducts(): Observable<any> {
+    const url = `${this.PRODUCT_API}`;
+    return this.httpClient.get<any>(url, this.httpOptions);
+  }
+}
