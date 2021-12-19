10d0af4d7e57dd773336218b12cb57fc7c99de8e
lqtoan <lqtoan37@gmail.com>
2021-12-19 10:27:10 +0700

    update

--- src/app/modules/product/product-detail/product-detail.component.ts
+++ src/app/modules/product/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { ProductService } from './../product.service';
-import { Product } from '../product';
+import { Product } from '../../../models/product';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
--- src/app/modules/product/product-list/product-list.component.ts
+++ src/app/modules/product/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Product } from '../product';
+import { Product } from '../../../models/product';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 
