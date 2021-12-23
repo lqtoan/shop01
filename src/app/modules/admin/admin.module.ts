31c64812d29188d6f8245eff44c3ef276878c802
lqtoan <lqtoan37@gmail.com>
2021-12-23 22:55:23 +0700

    switchMap

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { AdminRoutingModule } from './admin-routing.module';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { PipesModule } from 'src/app/core/pipes/pipes.module';
-import { AdminComponent } from './admin.component';
 import { CommonModule } from '@angular/common';
 import { ShareModule } from '../share.module';
 import { FormsModule } from '@angular/forms';
+
+import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [AdminComponent],
